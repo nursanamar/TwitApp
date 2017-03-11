70f0cb574032ce7c375145bc345a00a858890ea2
Nursan amar <nursan011@gmail.com>
2017-03-11 04:23:11 +0000

    halaman beranda bagian update

---
+++ resources/views/beranda.blade.php
@@ -0,0 +1,28 @@
+@extends("layouts.master")
+@section("js")
+@endsection
+@section("content")
+	<div class="row update">
+		<div class="container">
+		 	<div class="col-md-12">
+				<form>
+					<textarea class="form-control form-input" placeholder="Update status..."></textarea>
+					<div class="tombol-left"><input class="btn tombol " type="submit" value="submit"></div>
+				</form>
+			</div>
+		</div>
+	</div>
+	<div class="row status">
+		<div class="container">
+			<div class="col-md-12 col-sm-12 status-list">
+				<div class="col-md-2 col-xs-4 col-sm-4">
+					<img class="gambar col-sm-12" src="image/me.jpg"/>
+				</div>
+				<div class="col-md-10 col-xs-8 col-sm-8">
+					<h5>Teman </h5>
+					<p>jjkknbbhjbbvvhhhbb</p>
+				</div>
+			</div>
+		</div>
+	</div>
+@endsection
